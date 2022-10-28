01757b32dc6241ba0c482f8084c05eecd70e5beb
Cyrus <memsue@gmail.com>
2022-10-28 10:58:50 +0800

    add myapp

---
+++ main.js
@@ -0,0 +1,31 @@
+//import packages
+const express = require('express')
+const morgan= require('morgan')
+
+//configure port
+//check if it is set in the environment varible, if not then we default to 3000
+const port = process.env.port || 3000
+
+//create an instance of express application
+
+const app=express()
+
+//log all request
+app.use(morgan('common'))
+
+//get
+app.get('/',(req,resp)=>{
+
+    // what is the mdeia type
+    resp.type('text/html')
+    //set the statu code
+    resp.status(200)
+    //send the response
+    resp.send(`<h1>the current time is ${new Date()}</h1>`)
+})
+
+// start web application
+app.listen(port,()=>{
+    console.info(`starting application on port =${port} at ${new Date()}`)
+
+})
